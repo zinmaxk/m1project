aabe138635e9021044e3d113d5f8159f2086bb31
Kiet Mai <maianhkiet@gmail>
2019-08-03 23:53:53 +0700

    Handle 404 - Page not found

--- expressjs/server.js
+++ expressjs/server.js
@@ -25,8 +25,22 @@ if (environment !== 'production') {
     app.use(logger('dev'));
 }
 
+//The 404 Route (ALWAYS Keep this as the last route)
+// app.use(function(req, res, next) {
+//     // return res.status(404).send({ message: 'Route'+req.url+' Not found.' });
+// });
+
 const routes = require('./routes/index.js');
 app.use('/', routes(router));
+
+// Route not found (404)
+app.use(function(req, res, next) {
+    const page = {
+        pageTitle: 'Error!',
+        breadCrumbTitle: "Page not found"
+    };
+    return res.status(404).render('404error', {page: page});
+});
 
 app.listen(`${stage.port}`, () => {
     console.log(`Server now listening at http://localhost:${stage.port}`);
